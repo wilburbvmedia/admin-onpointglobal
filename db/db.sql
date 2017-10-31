a457edad281f5da05e7443f06d0d3f644e082c4a
Wilbur Powery <wilbur.powery@bvmedia.cr>
2017-10-31 08:00:56 -0600

    update db

--- db/db.sql
+++ db/db.sql
@@ -1,22 +1,22 @@
 CREATE TABLE applications(
   id_application    bigint(9) NOT NULL AUTO_INCREMENT,
-  name        varchar(100) NOT NULL,
-  email       varchar(50),
-  phone       int NOT NULL,
-  subject     varchar(50),
-  message     varchar(150),
-  cv         varchar(150),
-  domain      varchar(100),
+  name        varchar(128) NOT NULL,
+  email       varchar(64),
+  phone varchar(16),
+  subject     varchar(64),
+  message     varchar(128),
+  cv         varchar(256),
+  domain      varchar(128),
   PRIMARY KEY (id_application)
 );
 
 CREATE TABLE jobs(
   id_job      bigint(9) NOT NULL AUTO_INCREMENT,
-  date        varchar(50) NOT NULL,
-  job_title   text,
-  location    varchar(50),
-  job_type    varchar(50),
-  domain      varchar(100),
-  description  text,
+  date        date NOT NULL,
+  job_title   varchar(256),
+  location    varchar(64),
+  job_type    varchar(64),
+  domain      varchar(128),
+  description  longtext,
   PRIMARY KEY (id_job)
 );
